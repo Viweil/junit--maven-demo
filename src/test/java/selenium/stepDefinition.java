395770f6f31dad91521ac348fa1783b1b145572e
VI <moccababy777@gmail.com>
2017-08-13 19:50:04 +1200

    jenkins project

---
+++ src/test/java/selenium/stepDefinition.java
@@ -0,0 +1,26 @@
+package selenium;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class stepDefinition {
+	
+	WebDriver driver;
+	
+	@Given("^I go to \"([^\"]*)\"$")
+	public void i_go_to(String arg1) throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://supervi.net");
+	}
+
+	@Then("^I should be on \"([^\"]*)\" home page$")
+	public void i_should_be_on_home_page(String arg1) throws Throwable {
+	    Assert.assertTrue(driver.getTitle().contains("Super VI"));
+	    driver.close();
+	}
+}
---
+++ target/test-classes/selenium/cucumberFeature.feature
@@ -0,0 +1,5 @@
+Feature: Simple feature
+
+Scenario: Go to Super VI Home Page
+Given I go to "Super VI"
+Then I should be on "Super VI" home page
